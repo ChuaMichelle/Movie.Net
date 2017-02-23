f016e898c46d94f0c2bb31c7ff7ca0aa1c298c2d
ChuaMichelle <chuanarra.michelle@gmail.com>
2017-02-23 12:27:02 +0100

    complete tuto with style ( bindings )

---
+++ Movie.Net/Movie.Net/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace Movie.Net
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Name = "Hello MVVM!";
+            MyCommand = new RelayCommand(MycommandExecute, MyCommandCanExecute);
+        }
+
+        public string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set {
+                _name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand MyCommand { get; }
+
+        void MycommandExecute()
+        {
+            Name = "Hello click!";
+        }
+
+        bool MyCommandCanExecute()
+        {
+            return Name != "Hello click!";
+        }
+
+    }
+}
