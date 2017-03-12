209b5873525ea9c29ba30ad979b380fb7d3da93b
ChuaMichelle <chuanarra.michelle@gmail.com>
2017-03-12 18:48:40 +0100

    log out

--- Movie.Net/Movie.Net/ViewModel/MainViewModel.cs
+++ Movie.Net/Movie.Net/ViewModel/MainViewModel.cs
@@ -15,6 +15,7 @@ namespace Movie.Net.ViewModel
         public AuthenticationViewModel AuthViewModel { get; set; }
         public RelayCommand SignupCommand { get; set; }
         public RelayCommand LoginCommand { get; set; }
+        public RelayCommand LogoutCommand { get; set; }
         public User user = new User();
         private string _AuthErrorMessage;
         private bool _HasAuthErrorMessage;
@@ -26,6 +27,7 @@ namespace Movie.Net.ViewModel
             AuthViewModel = new AuthenticationViewModel();
             SignupCommand = new RelayCommand(SignupExecute, MyCommandsCanExecute);
             LoginCommand = new RelayCommand(LoginExecute, MyCommandsCanExecute);
+            LogoutCommand = new RelayCommand(LogoutExecute, MyCommandsCanExecute);
         }
 
         private bool MyCommandsCanExecute()
@@ -79,47 +81,66 @@ namespace Movie.Net.ViewModel
 
         private void SignupExecute()
         {
-            DataModelContainer ctx = new DataModelContainer();
-            if (ctx.Users.Any(u => u.Login == AuthViewModel.CurrentUser.Login))
+            if(AuthViewModel.IsNotAuthenticated)
             {
-                HandleMessages(false, "Error: Username already taken.");
+                DataModelContainer ctx = new DataModelContainer();
+                if (ctx.Users.Any(u => u.Login == AuthViewModel.CurrentUser.Login))
+                {
+                    HandleMessages(false, "Error: Username already taken.");
+                }
+                else
+                {
+                    ctx.Users.Add(AuthViewModel.CurrentUser);
+                    ctx.SaveChanges();
+                    HandleMessages(true, "Welcome ! Log in to continue to the app.");
+                }
             }
             else
             {
-                ctx.Users.Add(AuthViewModel.CurrentUser);
-                ctx.SaveChanges();
-                HandleMessages(true, "Welcome ! Log in to continue to the app.");
+                HandleMessages(false, "Can't sign up while user is logged.");
             }
         }
-
         private void LoginExecute()
-        {            
-            DataModelContainer ctx = new DataModelContainer();
-            if (ctx.Users.Any(u => u.Login == AuthViewModel.CurrentUser.Login))
+        {
+            if (AuthViewModel.IsNotAuthenticated)
             {
-                if (ctx.Users.Any(u => u.Login == AuthViewModel.CurrentUser.Login && u.Password == AuthViewModel.CurrentUser.Password))
+                DataModelContainer ctx = new DataModelContainer();
+                if (ctx.Users.Any(u => u.Login == AuthViewModel.CurrentUser.Login))
                 {
-                    if (AuthViewModel.IsNotAuthenticated)
+                    if (ctx.Users.Any(u => u.Login == AuthViewModel.CurrentUser.Login && u.Password == AuthViewModel.CurrentUser.Password))
                     {
-                        AuthViewModel.Authenticate();
-                        HandleMessages(true, "Hello! User logged in successfully!");
-                        var movieListWindow = new View.MovieListWindow();
-                        movieListWindow.ShowDialog();
+                        if (AuthViewModel.IsNotAuthenticated)
+                        {
+                            AuthViewModel.Authenticate();
+                            HandleMessages(true, "Hello! User logged in successfully!");
+                            var movieListWindow = new View.MovieListWindow();
+                            movieListWindow.ShowDialog();
+                        }
+                        else
+                        {
+                            HandleMessages(false, "Error: User already logged in.");
+                        }
                     }
                     else
                     {
-                        HandleMessages(false, "Error: User already logged in.");
+                        HandleMessages(false, "Error: Incorrect password.");
                     }
                 }
                 else
                 {
-                    HandleMessages(false, "Error: Incorrect password.");
-                }                
+                    HandleMessages(false, "Error:  User not found in the database.");
+                }
             }
             else
             {
-                HandleMessages(false, "Error:  User not found in the database.");
+                HandleMessages(false, "Wut. User is already logged ? lol");
             }
+        }
+
+        private void LogoutExecute()
+        {
+            AuthViewModel.LogOff();
+            HandleMessages(true, "Bye bye =)");
         }
 
         // false meanse error / true means success
